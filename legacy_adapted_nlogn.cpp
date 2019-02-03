28d1ac7a8c38cfb99d34ddddbba3d9388f2e4c77
kborozdin <borozdin.kirill@gmail.com>
2019-02-03 15:10:00 +0500

    Fix whitespaces

--- legacy_adapted_nlogn.cpp
+++ legacy_adapted_nlogn.cpp
@@ -23,62 +23,62 @@ using namespace std;
 
 namespace NLogN
 {
-	const int N = 3500500;
-	const int A = 26;
-	const int INF = (int)1e9;
+    const int N = 3500500;
+    const int A = 26;
+    const int INF = (int)1e9;
 
-	struct Eertree
-	{
-		int sz, last;
-		int go[N][A];
-		int len[N], diff[N];
-		int link[N], series_link[N];
-		int dp[2][N], last_dp[2][N];
+    struct Eertree
+    {
+        int sz, last;
+        int go[N][A];
+        int len[N], diff[N];
+        int link[N], series_link[N];
+        int dp[2][N], last_dp[2][N];
 
-		Eertree() : len(), diff(), link(), series_link(), dp(), last_dp()
-		{
-			sz = 2;
-			last = 0;
-			memset(go, -1, sizeof(go));
-			len[0] = -1;
+        Eertree() : len(), diff(), link(), series_link(), dp(), last_dp()
+        {
+            sz = 2;
+            last = 0;
+            memset(go, -1, sizeof(go));
+            len[0] = -1;
             for (int c : {0, 1})
                 fill(dp[c], dp[c] + N, INF);
-			dp[0][0] = 0;
-		}
+            dp[0][0] = 0;
+        }
 
-		int go_until_pal(const string &s, int i, int v)
-		{
-			while (s[i] != s[i - len[v] - 1])
-				v = link[v];
-			return v;
-		}
+        int go_until_pal(const string &s, int i, int v)
+        {
+            while (s[i] != s[i - len[v] - 1])
+                v = link[v];
+            return v;
+        }
 
-		void add_char(const string &s, int i)
-		{
-			last = go_until_pal(s, i, last);
-			int &v = go[last][s[i] - 'a'];
-			if (v == -1)
-			{
-				v = sz++;
-				len[v] = len[last] + 2;
-				if (last == 0)
-					link[v] = 1;
-				else
-				{
-					last = go_until_pal(s, i, link[last]);
-					link[v] = go[last][s[i] - 'a'];
-				}
-				diff[v] = len[v] - len[link[v]];
-				if (diff[v] == diff[link[v]])
-					series_link[v] = series_link[link[v]];
-				else
-					series_link[v] = link[v];
-			}
-			last = v;
-		}
+        void add_char(const string &s, int i)
+        {
+            last = go_until_pal(s, i, last);
+            int &v = go[last][s[i] - 'a'];
+            if (v == -1)
+            {
+                v = sz++;
+                len[v] = len[last] + 2;
+                if (last == 0)
+                    link[v] = 1;
+                else
+                {
+                    last = go_until_pal(s, i, link[last]);
+                    link[v] = go[last][s[i] - 'a'];
+                }
+                diff[v] = len[v] - len[link[v]];
+                if (diff[v] == diff[link[v]])
+                    series_link[v] = series_link[link[v]];
+                else
+                    series_link[v] = link[v];
+            }
+            last = v;
+        }
 
-		void calc_dp(int v, int i)
-		{
+        void calc_dp(int v, int i)
+        {
             for (int c : {0, 1})
             {
                 int baby_len = len[series_link[v]] + diff[v];
@@ -88,26 +88,26 @@ namespace NLogN
                 last_dp[1 - c][v] = res;
                 dp[c][i] = min(dp[c][i], res);
             }
-		}
-	};
+        }
+    };
 
-	Eertree eertree;
+    Eertree eertree;
 
-	vector<int> solve(string s)
-	{
-		int n = (int)s.length();
-		s = "#" + s;
-		vector<int> res[2];
+    vector<int> solve(string s)
+    {
+        int n = (int)s.length();
+        s = "#" + s;
+        vector<int> res[2];
 
-		for (int i = 1; i <= n; i++)
-		{
-			eertree.add_char(s, i);
-			for (int v = eertree.last; v != 1; v = eertree.series_link[v])
-				eertree.calc_dp(v, i);
+        for (int i = 1; i <= n; i++)
+        {
+            eertree.add_char(s, i);
+            for (int v = eertree.last; v != 1; v = eertree.series_link[v])
+                eertree.calc_dp(v, i);
             for (int c : {0, 1}) {
                 res[c].push_back(eertree.dp[c][i]);
             }
-		}
+        }
 
         vector<int> pal_len(res[0].size(), INF);
         for (int c : {0, 1})
@@ -123,34 +123,34 @@ namespace NLogN
                     throw;
             }
         return pal_len;
-	}
+    }
 }
 
 char buf[3500500];
 
 void solve()
 {
-	scanf("%s", buf);
-	string s(buf);
+    scanf("%s", buf);
+    string s(buf);
 
-	auto res = NLogN::solve(s);
-	for (auto e = res.begin(); e != res.end(); e++)
-		printf("%d ", *e);
-	printf("\n");
+    auto res = NLogN::solve(s);
+    for (auto e = res.begin(); e != res.end(); e++)
+        printf("%d ", *e);
+    printf("\n");
 }
 
 int main()
 {
 #ifdef LOCAL
-	freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
 #else
-	//freopen(".in", "r", stdin);
-	//freopen(".out", "w", stdout);
+    //freopen(".in", "r", stdin);
+    //freopen(".out", "w", stdout);
 #endif
 
-	solve();
+    solve();
 
-	eprintf("\n\ntime: %.3lf\n", (double)clock() / CLOCKS_PER_SEC);
-	return 0;
+    eprintf("\n\ntime: %.3lf\n", (double)clock() / CLOCKS_PER_SEC);
+    return 0;
 }
